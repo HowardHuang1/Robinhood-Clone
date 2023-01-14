5059a1a68cba09a144125079eb2a47365406e0cd
Howard Huang <howardhuang2004@gmail.com>
2023-01-14 15:51:54 -0800

    create newsfeed container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'; 
+import Newsfeed from './Newsfeed'
+import Stats from './Stats'
+
 
 function App() {
   return (
@@ -11,7 +14,11 @@ function App() {
       </div>
       {/* Body */}
       <div className="app__body">
-
+        <div className="app__container">
+          <Newsfeed />
+          {/* stats */}
+          <Stats />
+        </div>
       </div>
     </div>
   );
